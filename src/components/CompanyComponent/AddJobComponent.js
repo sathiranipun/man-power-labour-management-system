7e55278f566fbda3e9f5804abea39989dcfd6e7d
sathiranipun <sathiranipun.vsn@gmail.com>
2021-11-12 17:45:39 +0530

    company component updated

---
+++ src/components/CompanyComponent/AddJobComponent.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Button, Form } from "react-bootstrap";
+
+const AddJobComponent = () => {
+    return (
+        <div>
+            <Form className="p-4">
+                <Form.Group className="mb-3" controlId="formCompanyName">
+                    <Form.Label>Company Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Company Name" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formCompanyEmail">
+                <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formCompanyContact">
+                <Form.Label>Company Contact Number</Form.Label>
+                    <Form.Control type="number" placeholder="Enter Company Contact Number" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formCompanyAddress">
+                <Form.Label>Company Address</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Address" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    )
+}
+
+export default AddJobComponent
